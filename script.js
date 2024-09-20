c100b97a7087d749133664cb9a316af58c7a8131
Athul <athulea252@gmail.com>
2024-09-20 19:31:14 +0530

    final commit

--- script.js
+++ script.js
@@ -23,39 +23,39 @@ const cardData = [
     {
         image: "Images/sunny-rainy-cloudy-day-weather-forecast-icon-illustration_47987-10695.avif",
         title: "Reall data in Framer:weather",
-        description: "Become a super designer with Frame",
+        description: "imprting the open weather APi in override to use real weather data",
         date: "Jan 10, 2024",
         label: ""
     },
     {
         image:"Images/zmFoXJs0ldkq6CXQDSTAmQJWpGs.avif",
-        title: "Fantast",
-        description: "Become a super designer with Frame",
+        title: "Reall data in Framer:Clock",
+        description: "UseDate() in override to get The real time data",
         date: "Jan 10, 2024",
         label: ""
     },{
         image: "Images/spring.jpeg",
-        title: "Fantast",
-        description: "Become a super designer with Frame",
+        title: "About spring Animation",
+        description: "Take a deeper look at spring animation in frame motion",
         date: "Jan 10, 2024",
         label: ""
     },{
         image: "Images/aitool.webp",
-        title: "Fantast",
-        description: "Become a super designer with Frame",
-        date: "Jan 10, 2024",
+        title: "Framees",
+        description: "A tool is help you retouch images and frames",
+        date: "Jun 1, 2022",
         label: ""
     },{
         image: "Images/llyAb5sPQWhUbn6wcmJ29C3ktmk.avif",
-        title: "Fantast",
-        description: "Become a super designer with Frame",
-        date: "Jan 10, 2024",
+        title: "Wowpad OS",
+        description: "Prototype iPados running on the web",
+        date: "Jan 12, 2022",
         label: ""
     },{
         image:"Images/book.jpg" ,
-        title: "Fantast",
-        description: "Become a super designer with Frame",
-        date: "Jan 10, 2024",
+        title: "Dadeo AR",
+        description: "Discover how we are bringing your favorite books to AR world",
+        date: "Aug 1, 2020",
         label: ""
     }
     
